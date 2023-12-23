6061fc0a2b98249bfb31958bc305be9696d6702f
poquitodev <fromlatonyy@gmail.com>
2023-12-23 17:50:26 +0000

    rr

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'], strict_slashes=False)
+def hello_world():
+    return "<p>Hello HBNB!</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
